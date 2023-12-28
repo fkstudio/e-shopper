8c71c9a966edba3e539f49a18689258e2e1e20fd
Franklin Perez <fkop04@gmail.com>
2023-12-28 14:22:26 -0400

    slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,9 +32,24 @@
 
 
 /*=============== HOME SWIPER ===============*/
+var homeSwiper = new Swiper(".home-swiper", {
+    spaceBettween: 30,
+    loop: 'true',
+
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
 
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader() {
+    const header = document.getElementById('header')
+    // whenthe scrioll is greater than 50 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
 
 
 /*=============== NEW SWIPER ===============*/
