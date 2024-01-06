499b8a7b69f380f693e496d2cfd442fd731a9a56
Franklin Perez <fkop04@gmail.com>
2024-01-05 20:18:56 -0400

    ayuda FC

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,25 +10,47 @@
 
 
 /*=============== SHOW CART ===============*/
-
+const cart = document.getElementById('cart'),
+      cartShop = document.getElementById ('cart-shop'),
+      cartShop = document.getElementById('cart-close')
 
 /*===== CART SHOW =====*/
 /* Validate if constant exists */
-
+if(cartShop) {
+  cartShop.addEventListener("click", () => {
+    cart.classList.add('show-cart')
+  })
+}
 
 /*===== CART HIDDEN =====*/
 /* Validate if constant exists */
-
+if(cartClose) {
+  cartClose.addEventListener("click", () => {
+    cart.classList.remove('show-cart')
+  })
+}
 
 /*=============== SHOW LOGIN ===============*/
-
+const login = document.getElementById('login'),
+      loginButton = document.getElementById ('login-button'),
+      loginClose = document.getElementById('login-close')
 
 /*===== LOGIN SHOW =====*/
 /* Validate if constant exists */
-
+if(loginButton) {
+  loginButton.addEventListener("click", () => {
+    login.classList.add('show-login')
+  })
+}
 
 /*===== LOGIN HIDDEN =====*/
 /* Validate if constant exists */
+if(loginclose) {
+  loginclose.addEventListener("click", () => {
+    login.classList.add('show-login')
+  })
+}
+
 
 
 /*=============== HOME SWIPER ===============*/
@@ -46,7 +68,7 @@ var homeSwiper = new Swiper(".home-swiper", {
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 function scrollHeader() {
     const header = document.getElementById('header')
-    // whenthe scrioll is greater than 50 viewport height, add the scroll-header class to the header tag
+    // when the scrioll is greater than 50 viewport height, add the scroll-header class to the header tag
     if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
@@ -62,6 +84,11 @@ var newSwiper = new Swiper(".new-swiper", {
 
 
 /*=============== SHOW SCROLL UP ===============*/ 
+function scrollUp() {
+  const scrollUp = document.getElementById('scroll-up');
+   // when the scrioll is greater than 350 viewport height, add the scroll-scroll to a with the scroll-top class
+   if(this.scrollY >= 350) scrollUp.classsList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
 
 
 /*=============== LIGHT BOX ===============*/
